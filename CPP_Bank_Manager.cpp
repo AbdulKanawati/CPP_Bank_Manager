41804a68906d47d11c9f0b164b517adcb8d137fd
Abdul Kanawati <abode.knwaty@gmail.com>
2025-04-29 07:54:45 +0200

    Feat: add initial main menu screen for client management system

--- CPP_Bank_Manager.cpp
+++ CPP_Bank_Manager.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 using namespace std;
 
+// Show the main menu screen
+void ShowMainMenu()
+{
+    system("cls");
+    cout << "===========================================\n";
+    cout << "\t\tMain Menue Screen\n";
+    cout << "===========================================\n";
+    cout << "\t[1] Show Client List.\n";
+    cout << "\t[2] Add New Client.\n";
+    cout << "\t[3] Delete Client.\n";
+    cout << "\t[4] Update Client Info.\n";
+    cout << "\t[5] Find Client.\n";
+    cout << "\t[6] Transactions.\n";
+    cout << "\t[7] Exit.\n";
+    cout << "===========================================\n";
+}
+
 int main()
 {
-
+    ShowMainMenu(); // Start program by showing main menu
     return 0;
 }
