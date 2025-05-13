8fd8a9b0ef0ffd3fa23e7a5ce9c383e1517f2435
Abdul Kanawati <abode.knwaty@gmail.com>
2025-05-13 15:45:51 +0200

    Feat: add formatted output function for displaying client records

--- CPP_Bank_Manager.cpp
+++ CPP_Bank_Manager.cpp
@@ -95,11 +95,43 @@ vector <sClient> LoadCleintsDataFromFile(string FileName)
 
 }
 
+// Prints a formatted client record
+void PrintClientRecordLine(sClient Client)
+{
+    cout << "| " << setw(15) << left << Client.AccountNumber;
+    cout << "| " << setw(10) << left << Client.PinCode;
+    cout << "| " << setw(40) << left << Client.Name;
+    cout << "| " << setw(12) << left << Client.Phone;
+    cout << "| " << setw(12) << left << Client.AccountBalance;
+}
+
 void ShowAllClientsScreen()
 {
     vector <sClient> vClients = LoadCleintsDataFromFile(ClientsFileName);
-    // TODO 1: implement LoadCleintsDataFromFile
-    // TODO 2: Rest
+
+    cout << "\n\t\t\t\t\tClient List (" << vClients.size() << ") Client(s).";
+    cout << "\n_______________________________________________________";
+    cout << "_________________________________________\n" << endl;
+
+    cout << "| " << left << setw(15) << "Accout Number";
+    cout << "| " << left << setw(10) << "Pin Code";
+    cout << "| " << left << setw(40) << "Client Name";
+    cout << "| " << left << setw(12) << "Phone";
+    cout << "| " << left << setw(12) << "Balance";
+    cout << "\n_______________________________________________________";
+    cout << "_________________________________________\n" << endl;
+
+    if (vClients.size() == 0)
+        cout << "\t\t\t\tNo Clients Available In the System!";
+    else
+
+        for (sClient Client : vClients)
+        {
+            PrintClientRecordLine(Client);
+            // TODO: Rest after implement PrintClientRecordLine
+        }
+
+
 }
 // Go back to main menu
 void GoBackToMainMenu()
