5788005bf764c2c65038dcab5171b110f213da93
Abdul Kanawati <abode.knwaty@gmail.com>
2025-05-13 15:27:35 +0200

    Feat: implement parser to convert text line into client record

--- CPP_Bank_Manager.cpp
+++ CPP_Bank_Manager.cpp
@@ -61,7 +61,13 @@ sClient ConvertLinetoRecord(string Line, string Seperator = "#//#")
 {
     sClient Client;
     vector<string> vClientData = SplitString(Line, Seperator);
-    //TODO: Rest
+    Client.AccountNumber = vClientData[0];
+    Client.PinCode = vClientData[1];
+    Client.Name = vClientData[2];
+    Client.Phone = vClientData[3];
+    Client.AccountBalance = stod(vClientData[4]);//cast string to double
+
+    return Client;
 }
 
 // Loads all clients from file into a vector
